36f840c8ea8a98a0444cfcd7887b6dc0ccb25d5c
LiuYinCarl <bearcarl@qq.com>
2025-08-24 19:13:30 +0800

    remove unused function

--- main.go
+++ main.go
@@ -173,17 +173,6 @@ func dirMonitor() {
 	<-done
 }
 
-func Exists(path string) bool {
-	_, err := os.Stat(path) //os.Stat获取文件信息
-	if err != nil {
-		if os.IsExist(err) {
-			return true
-		}
-		return false
-	}
-	return true
-}
-
 func IsDir(path string) bool {
 	s, err := os.Stat(path)
 	if err != nil {
